5f3edaf5b4d1d22334dec21966deb3f27a520ec2
Aathi <aathithirunav@gmail.com>
2026-06-06 23:03:26 +0530

    Add MySQL connector and implement UserDAO for database connection

--- src/com/bus/dao/UserDAO.java
+++ src/com/bus/dao/UserDAO.java
@@ -1,9 +1,26 @@
 package com.bus.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import com.bus.model.User;
 
 public class UserDAO {
-    
+
+    private Connection con;
+
+    public UserDAO() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/your_database";
+            String user = "username";
+            String pass = "password";
+            con = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void registerUser(User user){
         
     }
