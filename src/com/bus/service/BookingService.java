3dbee8e8ceada5125282fc2e028ed01aa7f58dfa
Aathi <aathithirunav@gmail.com>
2026-06-07 12:51:09 +0530

    Implement booking and bus management methods in BookingService and BusService classes

--- src/com/bus/service/BookingService.java
+++ src/com/bus/service/BookingService.java
@@ -1,5 +1,17 @@
 package com.bus.service;
 
 public class BookingService {
+
+    public void bookTicket() {
+       
+    }
+
+    public void myBookings() {
+        
+    }
+
+    public void cancekBooking() {
+        
+    }
     
 }
--- src/com/bus/service/BusService.java
+++ src/com/bus/service/BusService.java
@@ -1,5 +1,25 @@
 package com.bus.service;
 
 public class BusService {
+
+    public void viewAllBuses() {
+        
+    }
+
+    public void addBus() {
+        
+    }
+
+    public void updateBus() {
+        
+    }
+
+    public void deleteBus() {
+        
+    }
+
+    public void searchBus() {
+        
+    }
     
 }
