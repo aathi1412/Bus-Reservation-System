76387c78b563297ff2dba0a57f157e481202b6ff
Aathi <aathithirunav@gmail.com>
2026-06-11 20:39:22 +0530

    Add custom exception classes for bus booking: BusHasBookingException and BusNotFoundException

---
+++ src/com/bus/exception/BusHasBookingException.java
@@ -0,0 +1,8 @@
+package com.bus.exception;
+
+public class BusHasBookingException extends Exception{
+    
+    public BusHasBookingException(String message){
+        super(message);
+    }
+}
---
+++ src/com/bus/exception/BusNotFoundException.java
@@ -0,0 +1,8 @@
+package com.bus.exception;
+
+public class BusNotFoundException extends Exception{
+    
+    public BusNotFoundException(String message){
+        super(message);
+    }
+}
