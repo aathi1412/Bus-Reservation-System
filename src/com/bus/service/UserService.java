5d007bdfea55622fa6d29cd13914d3737916fb77
Aathi <aathithirunav@gmail.com>
2026-06-19 22:42:58 +0530

    Add method to check for existing email in UserService

--- src/com/bus/service/UserService.java
+++ src/com/bus/service/UserService.java
@@ -1,5 +1,7 @@
 package com.bus.service;
 
+import java.sql.SQLException;
+
 import com.bus.dao.UserDAO;
 import com.bus.exception.AuthenticationException;
 import com.bus.model.User;
@@ -18,5 +20,13 @@ public class UserService {
 
     public User login(String email, String password) throws AuthenticationException{
         return userDAO.validateUser(email, password);
+    }
+
+    public boolean checkExistingEmail(String email) throws SQLException{
+        try {
+            return userDAO.checkExistingEmail(email);
+        } catch (SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
     }
 }
